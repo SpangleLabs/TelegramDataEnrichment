ffacd87a23e50b4aa9780ffa112cdc5014ccd21a
Joshua Coales <joshua@coales.co.uk>
2020-06-04 00:26:57 +0100

    Initial commit of TelegramDataEnrichment project

---
+++ MainClass.cs
@@ -0,0 +1,19 @@
+﻿using DreadBot;
+
+namespace TelegramDataEnrichment
+{
+    public class MainClass : IDreadBotPlugin
+    {
+        public string PluginID => "Data Enrichment Helper";
+
+        public void Init()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void PostInit()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TelegramDataEnrichment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TelegramDataEnrichment")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("A808AE9C-F6D3-4CF3-9C5A-47749D295D8D")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
