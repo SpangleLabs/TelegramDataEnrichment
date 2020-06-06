0f92cf577abee7a269a0bc49d207ab8e73ecdc08
Joshua Coales <joshua@coales.co.uk>
2020-06-06 13:09:22 +0100

    Moving Menus to a separate class

--- EnrichmentManager.cs
+++ EnrichmentManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using DreadBot;
 
 namespace TelegramDataEnrichment
@@ -70,101 +70,6 @@ namespace TelegramDataEnrichment
                 }
             } 
             menu.SendReply(eventArgs.msg.chat.id, eventArgs.msg.message_id);
-        }
-
-        private abstract class Menu
-        {
-            protected abstract string Text();
-            protected abstract InlineKeyboardMarkup Keyboard();
-
-            public void SendReply(long chatId, long messageId)
-            {
-                Methods.sendReply(chatId, messageId, Text(), keyboard: Keyboard());
-            }
-
-            public void EditMessage(long chatId, long messageId)
-            {
-                Methods.editMessageText(chatId, messageId, Text(), keyboard: Keyboard());
-            }
-        }
-
-        private class UnknownMenu : Menu
-        {
-            private readonly string _callbackData;
-
-            public UnknownMenu(string callbackData)
-            {
-                _callbackData = callbackData;
-            }
-            
-            protected override string Text()
-            {
-                return $"I do not understand this callback data yet: {_callbackData}";
-            }
-
-            protected override InlineKeyboardMarkup Keyboard()
-            {
-                var keyboard = new InlineKeyboardMarkup();
-                keyboard.addCallbackButton("🔙", RootMenu.CallbackName, 0);
-                return keyboard;
-            }
-        }
-
-        private class RootMenu : Menu
-        {
-            private readonly List<EnrichmentSession> _sessions;
-            public const string CallbackName = "menu";
-
-            public RootMenu(List<EnrichmentSession> sessions)
-            {
-                _sessions = sessions;
-            }
-
-            protected override string Text()
-            {
-                var activeSessions = _sessions.FindAll(s => s.IsActive).Count;
-                return "Welcome to the enrichment system menu. " +
-                       $"There are {_sessions.Count} configured sessions, and {activeSessions} are active.";
-            }
-
-            protected override InlineKeyboardMarkup Keyboard()
-            {
-                var keyboard = new InlineKeyboardMarkup();
-                keyboard.addCallbackButton("Create new session", "session_create", 0);
-                keyboard.addCallbackButton("Start session", "session_start", 1);
-                keyboard.addCallbackButton("End session", "session_end", 2);
-                keyboard.addCallbackButton("Delete session", "session_delete", 3);
-                return keyboard;
-            }
-        }
-
-        private class StartSessionMenu : Menu
-        {
-            private readonly List<EnrichmentSession> _sessions;
-            public const string CallbackName = "session_start";
-
-            public StartSessionMenu(List<EnrichmentSession> sessions)
-            {
-                _sessions = sessions;
-            }
-
-            protected override string Text()
-            {
-                return "Which enrichment session would you like to start?";
-            }
-
-            protected override InlineKeyboardMarkup Keyboard()
-            {
-                var inActiveSessions = _sessions.FindAll(s => !s.IsActive);
-                var keyboard = new InlineKeyboardMarkup();
-                var row = 0;
-                foreach (var session in inActiveSessions)
-                {
-                    keyboard.addCallbackButton(session.Name, $"session_start {session.Name}", row++);
-                }
-                keyboard.addCallbackButton("🔙", RootMenu.CallbackName, row);
-                return keyboard;
-            }
         }
     }
 }
